c867e0e1c1b78aa966060a4822763409cdc713d9
jakekasan <jake.kasan@gmail.com>
2018-09-29 22:22:10 +0100

    fixed imports and started working on News_Source class

--- news_source/main.py
+++ news_source/main.py
@@ -1,8 +1,8 @@
-from data_handler import Data_Handler
-from news_wrangler import News_Wrangler
-from guardian import Guardian
-from config import config
-from ticker_lookup import ticker_lookup
+from news_source.tools.data_handler import Data_Handler
+from news_source.sources.news_wrangler import News_Wrangler
+from news_source.sources.guardian import Guardian
+from config import news_sources as config
+from news_source.tools.ticker_lookup import ticker_lookup
 
 import datetime as dt
 import numpy as np
@@ -14,49 +14,52 @@ class News_Source():
     def __init__(self,df=None,date_col=None):
         pass
 
+    def get_text_data(self,df=None,date_col=None):
+        pass
 
 
 
 
-def main():
 
-    pwd = os.getcwd()
+# def main():
 
-    if "data" not in [x for x in os.listdir(pwd) if os.path.isdir(x)]:
-        print("No data directory")
-        return
+#     pwd = os.getcwd()
 
-    data_dir = os.path.join(pwd,"data")
+#     if "data" not in [x for x in os.listdir(pwd) if os.path.isdir(x)]:
+#         print("No data directory")
+#         return
 
-    csv_regex = re.compile(r"(.*)\.csv")
+#     data_dir = os.path.join(pwd,"data")
 
-    companies = {}
+#     csv_regex = re.compile(r"(.*)\.csv")
 
-    for f in os.listdir(data_dir):
-        match = csv_regex.match(f)
-        companies[match.group(1)] = str(match.group())
+#     companies = {}
 
-    for company,filename in companies.items():
-        df = pd.read_csv(os.path.join("./data",filename))
-        print("Read data for {}".format(company))
-        print(df.head())
+#     for f in os.listdir(data_dir):
+#         match = csv_regex.match(f)
+#         companies[match.group(1)] = str(match.group())
 
-    #ticker_lookup("RBS")
-    return
+#     for company,filename in companies.items():
+#         df = pd.read_csv(os.path.join("./data",filename))
+#         print("Read data for {}".format(company))
+#         print(df.head())
 
-    df["date"] = df["Date"].apply(lambda x: dt.datetime.strptime(x,"%Y-%M-%d"))
+#     #ticker_lookup("RBS")
+#     return
 
-    print(df.head())
+#     df["date"] = df["Date"].apply(lambda x: dt.datetime.strptime(x,"%Y-%M-%d"))
 
-    df["name"] = df.apply(lambda x: "RBS",axis=1)
+#     print(df.head())
 
-    df = df.head(20)
+#     df["name"] = df.apply(lambda x: "RBS",axis=1)
 
-    dh = Data_Handler()
+#     df = df.head(20)
 
-    results = dh.run(df)
+#     dh = Data_Handler()
 
-    print(results.tail(1)["raw_text_guardian"].values)
+#     results = dh.run(df)
+
+#     print(results.tail(1)["raw_text_guardian"].values)
 
 if __name__ == '__main__':
     main()
