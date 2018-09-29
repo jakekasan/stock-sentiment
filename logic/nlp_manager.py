0df57bdc7078c70505d8472ebb4be7df8ac456cf
jakekasan <jake.kasan@gmail.com>
2018-09-29 22:21:47 +0100

    added nlp_manager for root functionality

---
+++ logic/nlp_manager.py
@@ -0,0 +1,17 @@
+from data.data_importer import Data_Importer
+from news_source.tools.data_handler import Data_Handler
+
+class NLP_Manager:
+    def __init__(self):
+        pass
+        
+    def run(self):
+        """
+            main function
+        """
+
+        dh = Data_Handler()
+        
+
+
+
