90960b93fe54fb93e9a2e8bee26c85a1ad68581a
jakekasan <jake.kasan@gmail.com>
2018-09-29 21:13:21 +0100

    added info and price addresses for world_trading_data

--- config/data_sources.py
+++ config/data_sources.py
@@ -1,4 +1,7 @@
 world_trading_data = {
-    "address":"https://www.worldtradingdata.com/api/v1/stock",
+    "address":{
+        "info": "https://www.worldtradingdata.com/api/v1/stock",
+        "price": "https://www.worldtradingdata.com/api/v1/history"
+        },
     "api_key":"eCK0L4fxRNlDXtZYTrn7m7lr7Ps1oLc3JJ8uMxhcRls8XZOS0VFZcWtnFeK3",
 }
