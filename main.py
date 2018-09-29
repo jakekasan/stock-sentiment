4aa76b6cc3306f8496dd4e7a23b79ef1e5456504
jakekasan <jake.kasan@gmail.com>
2018-09-29 21:13:05 +0100

    testing Data_Importer

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-#news_source = exec(open("..news-source/__init__.py").read())
+#!/usr/local/bin/python3
 
 from news_source.tools.data_handler import Data_Handler
+from data.data_importer import Data_Importer
 
 import subprocess
 
 def main():
-    
+    di = Data_Importer()
+    result = di.search("RBS")
+    print(result)
     return
 
 if __name__ == '__main__':
