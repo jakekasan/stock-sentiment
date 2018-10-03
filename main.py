4c411dbf97e2cfcc0f67ae3e630b923731920677
jakekasan <jake.kasan@gmail.com>
2018-10-03 21:04:33 +0100

    added nlp_manager to alternative main function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from news_source.tools.data_handler import Data_Handler
 from data.data_importer import Data_Importer
 from data.data_importer import get_prices
+from logic.nlp_manager import NLP_Manager
 
 import numpy as np
 
@@ -22,7 +23,15 @@ def loaded_main():
     
     dh.get_returns()
 
+    dh.make_label()
+
     results = dh.get_df()
+
+    nlp = NLP_Manager(df = results)
+
+    nlp.get_all_words()
+
+    print(results.head())
 
     #print(np.sum(results["raw_text_guardian"] == ""))
     print(np.sum(results["raw_text_guardian"].apply(lambda x: type(x) == str)))
