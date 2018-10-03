45eec1154753e2de6989d9a71ae519056bd9a3d2
jakekasan <jake.kasan@gmail.com>
2018-10-03 16:46:31 +0100

    added debugging and static functions

--- data/data_importer.py
+++ data/data_importer.py
@@ -6,14 +6,93 @@ import requests
 
 
 class Data_Importer:
-    def __init__(self,name=None):
-        self.name = name
+    def __init__(self,debug=False):
+        self.data = {}
+        self.debug = debug
 
-    def set_name(self,name=None):
-        self.name = name
+    def load_data(self,name=None,date_from=None,date_to=None):
+        """
+            returns a DataFrame with the stock closing prices
+        """
 
-    def get_data(self,name=None,date_from=None,date_to=None):
+        if self.debug:
+            print("Starting data import...")
+            print("Name: {}".format(name))
+            print("Date From: {}".format(date_from))
+            print("Date To: {}".format(date_to))
+
         results = self.search(name=name,date_from=date_from,date_to=date_to)
+
+        if results is None:
+            return None
+
+        price = [item["close"] for key,item in results.items()]
+        date = [key for key,item in results.items()]
+
+        df = pd.DataFrame({"date":date,"price":price})
+
+        self.data[name] = df
+
+        if self.debug:
+            print("Data preview...")
+            print(df.head())
+
+        return df
+
+    def search(self,name=None,date_from=None,date_to=None):
+        try:
+            if self.debug:
+                print("Starting search for stock {}...".format(name))
+            if name is None:
+                return None
+
+            date_to = date_to or dt.datetime.now()
+            date_from = date_from or date_to - dt.timedelta(days=365)
+
+            # format dates
+            
+
+            date_to = date_to.strftime("%Y-%m-%d")
+            date_from = date_from.strftime("%Y-%m-%d")
+            
+            if self.debug:
+                print("Date from set at {}\nDate to set at {}\n".format(date_from,date_to))
+
+            params = {
+                "api_token":world_trading_data["api_key"],
+                "sort":"newest",
+                "symbol":name,
+                "date_from":date_from,
+                "date_to":date_to
+            }
+
+            r = requests.get(world_trading_data["address"]["price"],params=params)
+
+            return r.json()["history"]    
+
+        except:
+            if self.debug:
+                print("Encountered problem searching for stock {}".format(name))
+
+            return None
+
+
+    def error(self):
+        def no_name():
+            return print("No name given to search for")
+        def wrong_date_format():
+            return print("Date must either be None or a datetime object")
+
+        return {
+            no_name,
+            wrong_date_format
+        }
+
+    # Non OO way ( classes are pointless here... )
+
+
+def get_prices(name=None,date_from=None,date_to=None):
+        results = search(name=name,date_from=date_from,date_to=date_to)
 
         if results is None:
             return None
@@ -25,9 +104,9 @@ class Data_Importer:
 
         print(df.head())
 
-        return None
+        return df
 
-    def search(self,name=None,date_from=None,date_to=None):
+def search(name=None,date_from=None,date_to=None):
         try:
             if name is None:
                 return None
@@ -49,15 +128,3 @@ class Data_Importer:
             return r.json()["history"]    
         except:
             return None
-
-
-    def error(self):
-        def no_name():
-            return print("No name given to search for")
-        def wrong_date_format():
-            return print("Date must either be None or a datetime object")
-
-        return {
-            no_name,
-            wrong_date_format
-        }
