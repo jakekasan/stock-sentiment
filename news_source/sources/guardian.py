7bf5305da276786151df2147765b17ce019588d5
jakekasan <jake.kasan@gmail.com>
2018-10-05 12:07:06 +0100

    added debugging and fixed 'tags' param - still needs work

--- news_source/sources/guardian.py
+++ news_source/sources/guardian.py
@@ -5,13 +5,14 @@ from news_source.sources.news_wrangler import News_Wrangler
 address = "https://content.guardianapis.com/search"
 
 class Guardian(News_Wrangler):
-    def __init__(self):
+    def __init__(self,debug=False):
         super()
+        self.debug = debug
         self.name = "guardian"
         self.address = api["address"]
         self.API_KEY = api["API_KEY"]
 
-    def api_search(self,search,from_date=None,to_date=None,news_delay=None):
+    def api_search(self,search,from_date=None,to_date=None,news_delay=None,just_results=False):
         """
             API-specific search function
 
@@ -24,18 +25,25 @@ class Guardian(News_Wrangler):
 
         params = {
             "api-key":self.API_KEY,
-            "q":search,
+            "q":"{} OR (royal AND bank AND of AND scotland) OR (brexit) OR (mark AND carney) OR economy OR unemployment OR".format(search),
             "from-date":from_date,
             "to-date":to_date,
             "show-fields":"bodyText",
-            "tags":"business"
+            "tags":"business/business",
+            "page-size":50
         }
 
         try:
             r = requests.get(self.address,params=params)
             results = r.json()
 
+            if self.debug:
+                print(r.url)
+
             articles = results["response"]["results"]
+
+            if just_results:
+                return articles
 
             return self.process_result(articles)
 
